dc68b02f6f41f8dafd333b971092a043dff3bd25
Uridyner <152835266+Uridyner@users.noreply.github.com>
2024-05-03 08:54:10 -0300

    change to tp3

--- Blas-Strambi-TP3-DAP/tp3_screens2/lib/main.dart
+++ Blas-Strambi-TP3-DAP/tp3_screens2/lib/main.dart
@@ -12,7 +12,6 @@ class MainApp extends StatelessWidget {
 
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(); 
-    );
+      home: LoginScreen()); 
   }
 }
