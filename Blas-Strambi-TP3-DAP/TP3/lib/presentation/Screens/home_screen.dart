266c1df213ce44b9a89c41b890a366f9fe30135d
BlasStr <blas.e.strambi@gmail.com>
2024-05-03 00:46:23 -0300

    changes to login

---
+++ Blas-Strambi-TP3-DAP/TP3/lib/presentation/Screens/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class HomeScreen extends StatelessWidget {
+static const String name = 'home';
+
+String userName;
+ HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return  Scaffold(
+      appBar: AppBar (
+        title: const Text('Home')
+        ),
+      body:  Center(
+        child: Column (mainAxisAlignment: MainAxisAlignment.center,
+            
+            children: [
+              const Text('Bienvenido', style: TextStyle(fontSize: 40),),
+               Text($textoingresadouser, style: const TextStyle(color: Color.fromRGBO(255, 0, 0, 0), fontSize: 40),),
+          ],),
+      ),
+    );
+  }
+}
