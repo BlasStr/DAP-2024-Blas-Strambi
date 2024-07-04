0b2fb13defd888965dd107e067f1a0d2a2b988fa
BlasStr <blas.e.strambi@gmail.com>
2024-07-04 00:08:58 -0300

    rty

---
+++ TP2-Classes-ListView/TP-ListView/tp_listview/lib/core/data/games_datasource.dart
@@ -0,0 +1,39 @@
+import 'package:tp_listview/screens/home_screen.dart';
+
+final phoneGamesList = [
+  PhoneGames(
+    id: '1',
+    title: 'Brawl Stars',
+    description: 'Description goes here',
+    developer: 'Supercell',
+    year: 2017,
+    poster: 'https://i.scdn.co/image/ab6761610000e5ebf7b952107c126c561c52171e',
+  ),
+  PhoneGames(
+    id: '2',
+    title: 'Clash Royale',
+    description: 'Description goes here',
+    developer: 'Supercell',
+    year: 2016,
+    poster:
+        'https://www.gamersoft.net/wp-content/uploads/2023/06/clash-royale.webp',
+  ),
+  PhoneGames(
+    id: '3',
+    title: 'Subway Surfers',
+    description: 'Description goes here',
+    developer: 'SYBO Games',
+    year: 2012,
+    poster:
+        'https://play-lh.googleusercontent.com/alCSQLIJVL4VB9OtGATP7f503VojDSgl_V78Zzz8AvGxfs1xaEmIFoVWrKtjigjPMecH',
+  ),
+  PhoneGames(
+    id: '4',
+    title: 'Candy Crush',
+    description: 'Description goes here',
+    developer: 'King',
+    year: 2012,
+    poster:
+        'https://play-lh.googleusercontent.com/6ducfNys46MGge6Bl3kaHaGbNnnQh46j6-3JkOqk0PKgUNVhm-dlNenJwz65nYZ4B0g8',
+  ),
+];
